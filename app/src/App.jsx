959e00189678911ca45aa57cbea1fa5e99689b1e
Dmitry-Rar <rekhtin_dmitry@mail.ru>
2024-07-11 17:26:42 +0300

    update

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,5 @@
-
-import './styles/App.css';
-import Header from './components/Header.jsx';
+import "./styles/App.css";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
