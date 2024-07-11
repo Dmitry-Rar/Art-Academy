0e43963b7cc2c1d8d3e5d64a8faee8e7880a4f27
Dmitry-Rar <rekhtin_dmitry@mail.ru>
2024-07-11 17:26:12 +0300

    update: initial commit

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './styles/App.css';
+import Header from './components/Header.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import '../styles/Header.css';
+
+export default function Header(){
+    return(
+        <header className='header'>
+            <h1>H1</h1>
+        </header>
+    )
+}
